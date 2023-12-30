8470ad713f8108478af9cb2aa6e59ccbfc194123
CrezyDeveloper <122361875+CrezyDeveloper@users.noreply.github.com>
2023-12-30 12:44:36 +0530

    Add files via upload

---
+++ helper/txt.py
@@ -0,0 +1,52 @@
+
+class mr(object):
+    PROGRESS_BAR = """\n
+╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
+┣⪼ 🗂️ : {1} | {2}
+┣⪼ ⏳️ : {0}%
+┣⪼ 🚀 : {3}/s
+┣⪼ ⏱️ : {4}
+╰━━━━━━━━━━━━━━━➣ """
+
+    ABOUT_TXT = """
+╭───────────⍟
+├🤖 My Name : {}
+├👑 Developer : <a href=https://t.me/crezyBotz>Click Here</a> 
+├👨‍💻 Programer : <a href=https://github.com/CrezyDeveloper/Telegram-File-Renamer-Bot>Click Here</a>
+├📕 Library : <a href=https://github.com/pyrogram>Pyrogram</a>
+├✏️ Language : <a href=https://www.python.org>Python 3</a>
+├💾 Database : <a href=https://cloud.mongodb.com>MongoDB</a>
+├🌀 My Server : <a href=https://dashboard.render.com>Render</a>
+├📊 Build Status : v3.6.8             
+╰───────────────⍟
+                                """
+    HELP_TXT = """
+ <b><u>How To Set Thumbnile</u></b>
+  
+•> /start a bot and send any picture to automatically set thumbnile.
+•> /delthumb use this command and delete your old thumbnile.
+•> /viewthumb use this command view your current thumbnile.
+
+ <b><u>How To Set Custom Caption</u></b>
+•> /set_caption - set a custom caption
+•> /see_caption - see your custom caption
+•> /del_caption - delete custom caption
+
+Example:- /set_caption 📕 File Name: {filename}
+💾 Size: {filesize}
+⏰ Duration: {duration}
+
+✏️ <b><u>How To Rename A Files</u></b>
+•> send any file and click rename option and type new file name and \n send select [ document, video, audio ]👈 choice this.
+ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/CrezyBotz>support Group</a>
+"""
+
+#⚠️ don't remove our credits 🙏😢😢
+    DEV_TXT = """
+<b><u>Special Thanks & Developers</b></u> 
+
+» Source Code: <a href=https://github.com/CrezyDeveloper/Telegram-File-Renamer-Bot>Rename Bot</a>
+
+• ❣️ <a href=https://t.me/only_botz>OnlyBotz</a>
+• ❣️ <a href=https://t.me/crezybotz>CrezyBotz</a>
+"""
