51a4e74ccf56d92b52e9c4cf9113d7e52313c7e6
CrezyDeveloper <122361875+CrezyDeveloper@users.noreply.github.com>
2023-12-30 12:46:28 +0530

    Add files via upload

---
+++ plugins/start.py
@@ -0,0 +1,121 @@
+from asyncio import sleep
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
+from pyrogram.errors import FloodWait
+import humanize
+import random
+from helper.txt import mr
+from helper.database import db
+from config import START_PIC, FLOOD, ADMIN 
+
+
+@Client.on_message(filters.private & filters.command(["start"]))
+async def start(client, message):
+    user = message.from_user
+    if not await db.is_user_exist(user.id):
+        await db.add_user(user.id)             
+    txt=f"👋 {user.mention} \n I Am Simply File Rename+File To Video Converter Bot With Permanent Thumbnail & Custom Caption Support!"
+    button=InlineKeyboardMarkup([[
+        InlineKeyboardButton("Dev", callback_data='dev')
+        ],[
+        InlineKeyboardButton('Updates', url='https://t.me/only_botz'),
+        InlineKeyboardButton('Support', url='https://t.me/crezybotz')
+        ],[
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Help', callback_data='help')
+        ]])
+    if START_PIC:
+        await message.reply_photo(START_PIC, caption=txt, reply_markup=button)       
+    else:
+        await message.reply_text(text=txt, reply_markup=button, disable_web_page_preview=True)
+    
+
+@Client.on_message(filters.command('logs') & filters.user(ADMIN))
+async def log_file(client, message):
+    try:
+        await message.reply_document('TelegramBot.log')
+    except Exception as e:
+        await message.reply_text(f"Error:\n`{e}`")
+
+@Client.on_message(filters.private & (filters.document | filters.audio | filters.video))
+async def rename_start(client, message):
+    file = getattr(message, message.media.value)
+    filename = file.file_name
+    filesize = humanize.naturalsize(file.file_size) 
+    fileid = file.file_id
+    try:
+        text = f"""**__What do you want me to do with this file.?__**\n\n**File Name** :- `{filename}`\n\n**File Size** :- `{filesize}`"""
+        buttons = [[ InlineKeyboardButton("📝 Start Rename 📝", callback_data="rename") ],
+                   [ InlineKeyboardButton("✖️ Cancel ✖️", callback_data="cancel") ]]
+        await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
+        await sleep(FLOOD)
+    except FloodWait as e:
+        await sleep(e.value)
+        text = f"""**__What do you want me to do with this file.?__**\n\n**File Name** :- `{filename}`\n\n**File Size** :- `{filesize}`"""
+        buttons = [[ InlineKeyboardButton("📝 Start Rename 📝", callback_data="rename") ],
+                   [ InlineKeyboardButton("✖️ Cancel ✖️", callback_data="cancel") ]]
+        await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
+    except:
+        pass
+
+@Client.on_callback_query()
+async def cb_handler(client, query: CallbackQuery):
+    data = query.data 
+    if data == "start":
+        await query.message.edit_text(
+            text=f"""👋 {query.from_user.mention} \nI Am Simply File Rename+File To Video Converter Bot With Permanent Thumbnail & Custom Caption Support! """,
+            reply_markup=InlineKeyboardMarkup( [[
+                InlineKeyboardButton("Dev", callback_data='dev')                
+                ],[
+                InlineKeyboardButton('Updates', url='https://t.me/only_botz'),
+                InlineKeyboardButton('Support', url='https://t.me/crezybotz')
+                ],[
+                InlineKeyboardButton('About', callback_data='about'),
+                InlineKeyboardButton('Help', callback_data='help')
+                ]]
+                )
+            )
+    elif data == "help":
+        await query.message.edit_text(
+            text=mr.HELP_TXT,
+            reply_markup=InlineKeyboardMarkup( [[
+               #⚠️ don't change source code & source link ⚠️ #
+               InlineKeyboardButton("Back", callback_data = "start"),
+               InlineKeyboardButton("Close", callback_data = "close")
+               ]]
+            )
+        )
+    elif data == "about":
+        await query.message.edit_text(
+            text=mr.ABOUT_TXT.format(client.mention),
+            disable_web_page_preview = True,
+            reply_markup=InlineKeyboardMarkup( [[
+               #⚠️ don't change source code & source link ⚠️ #
+               InlineKeyboardButton("Back", callback_data = "start"),
+               InlineKeyboardButton("Close", callback_data = "close")
+               ]]
+            )
+        )
+    elif data == "dev":
+        await query.message.edit_text(
+            text=mr.DEV_TXT,
+            reply_markup=InlineKeyboardMarkup( [[
+               #⚠️ don't change source code & source link ⚠️ #
+               InlineKeyboardButton("Source Code", url="https://github.com/CrezyDeveloper/Telegram-File-Renamer-Bot")
+               ],[
+               InlineKeyboardButton("Back", callback_data = "start"),
+               InlineKeyboardButton("Close", callback_data = "close")
+               ]]
+            )
+        )
+    elif data == "close":
+        try:
+            await query.message.delete()
+            await query.message.reply_to_message.delete()
+        except:
+            await query.message.delete()
+
+
+
+
+
